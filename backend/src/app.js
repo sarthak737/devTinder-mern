33c1debbf1f1dbd20f489cf2b2b7ec8f270cfa1c
sarthak737 <sarthakgupta2537@gmail.com>
2025-02-26 17:24:04 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = new express();
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const { authRouter } = require("./routes/auth.js");
 const { profileRouter } = require("./routes/profile.js");
@@ -10,6 +11,7 @@ const { userRouter } = require("./routes/user.js");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/auth", authRouter);
 app.use("/user", profileRouter);
