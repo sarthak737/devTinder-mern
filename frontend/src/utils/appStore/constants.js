6e75e4dd749cb848b72b0d3f49f68e69d98bd662
sarthak737 <sarthakgupta2537@gmail.com>
2026-02-11 21:39:50 +0530

    fix deployment issue

--- frontend/src/utils/appStore/constants.js
+++ frontend/src/utils/appStore/constants.js
@@ -1,6 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
-export const BASE_URL = `${API_URL}/auth`;
-export const PROFILE_URL = `${API_URL}/profile`;
-export const USER_URL = `${API_URL}/user`;
-export const REQ_URL = `${API_URL}/request`;
+export const BASE_URL = `${import.meta.env.VITE_BASE_URL}/auth`;
+export const PROFILE_URL = `${import.meta.env.VITE_BASE_URL}/profile`;
+export const USER_URL = `${import.meta.env.VITE_BASE_URL}/user`;
+export const REQ_URL = `${import.meta.env.VITE_BASE_URL}/request`;
